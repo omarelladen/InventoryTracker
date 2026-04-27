bed488acec8a7bb8e7652c25b8e416e03283a22d
Omar Zagonel El Laden <omarzladen@gmail.com>
2026-04-26 23:08:07 -0300

    add basic rules to beep after frequent wakeups

--- firmware/main/main.ino
+++ firmware/main/main.ino
@@ -1,9 +1,15 @@
+#include <sys/time.h>
+
 #define PIN_WAKEUP 4
 #define PIN_LED 23
 #define PIN_BUZZER 22
 
+#define TIME_LIM_REP_S 10
+
 
 RTC_DATA_ATTR int boot_count = 0;
+RTC_DATA_ATTR int fast_wakeup_count = 0;
+RTC_DATA_ATTR unsigned long last_time_awake = 0;
 
 
 void beep_buzzer()
@@ -31,6 +37,44 @@ void setup()
     esp_sleep_enable_ext0_wakeup((gpio_num_t)PIN_WAKEUP, LOW);
 
 
+    // Time
+
+    time_t time_now;
+    time(&time_now);
+
+    unsigned long time_diff = time_now - last_time_awake;
+
+    Serial.print("Time: ");
+    Serial.println(time_now);
+    Serial.print("Last time awake: ");
+    Serial.println(last_time_awake);
+    Serial.print("Diff: ");
+    Serial.println(time_diff);
+
+    if (time_diff < TIME_LIM_REP_S)
+    {
+        fast_wakeup_count++;
+        Serial.print("Repeated wakeup! Count: ");
+        Serial.println(fast_wakeup_count);
+    }
+    else
+    {
+        // Reset count
+        fast_wakeup_count = 1;
+        Serial.println("Normal wakeup");
+    }
+
+    last_time_awake = time_now;
+
+    if (fast_wakeup_count > 3)  // repeated event
+    {
+        Serial.println("WARNING: repeated wakeup!");
+
+        // Alert
+        beep_buzzer();
+    }
+
+
     // Wakeup actions
 
     boot_count++;
@@ -40,8 +84,6 @@ void setup()
     delay(1000);  // delay to avoid multiple presses
     digitalWrite(PIN_LED, LOW);
     delay(300);
-
-    beep_buzzer();
 
     // Sleep
 
